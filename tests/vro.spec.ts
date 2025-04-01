224efaea4f1acd683b5e05176896c64818a564c4
Vilius Roškus <vroskus@gmail.com>
2025-04-01 14:37:04 +0300

    Added item creation test

--- tests/vro.spec.ts
+++ tests/vro.spec.ts
@@ -2,13 +2,67 @@ import { test, expect } from '@playwright/test';
 
 const pagePath = 'http://localhost:4200';
 
+// test(
+//   'main should be visible',
+//   async ({
+//     page,
+//   }) => {
+//     await page.goto(pagePath);
+
+//     await expect(page.locator('main')).toBeVisible();
+//   },
+// );
+
+// test(
+//   'list should be visible',
+//   async ({
+//     page,
+//   }) => {
+//     await page.goto(pagePath);
+
+//     await expect(page.locator('ul.List')).toBeVisible();
+//   },
+// );
+
+// test(
+//   'input should be visible',
+//   async ({
+//     page,
+//   }) => {
+//     await page.goto(pagePath);
+
+//     await expect(page.locator('input.ListItem-input')).toBeVisible();
+//   },
+// );
+
+// test(
+//   'add button should be visible',
+//   async ({
+//     page,
+//   }) => {
+//     await page.goto(pagePath);
+
+//     await expect(page.locator('button.ListItem-add')).toBeVisible();
+//   },
+// );
+
 test(
-  'page should be visible',
+  'added item should be visible',
   async ({
     page,
   }) => {
     await page.goto(pagePath);
 
-    await expect(page.locator('main')).toBeVisible();
+    const value = 'some random text';
+
+    await expect(page.locator('input')).toBeVisible();
+
+    await page.locator('input').fill(value);
+
+    await expect(page.getByRole('button')).toBeVisible();
+
+    await page.getByRole('button').click();
+
+    await expect(page.locator('li')).toHaveText(value);
   },
 );
