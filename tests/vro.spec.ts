5b6a6b94f305e1c53879cada3e0cd3d2c57bba5f
Vilius Roškus <vroskus@gmail.com>
2025-04-01 14:44:34 +0300

    Minor test update

--- tests/vro.spec.ts
+++ tests/vro.spec.ts
@@ -2,50 +2,6 @@ import { test, expect } from '@playwright/test';
 
 const pagePath = 'http://localhost:4200';
 
-// test(
-//   'main should be visible',
-//   async ({
-//     page,
-//   }) => {
-//     await page.goto(pagePath);
-
-//     await expect(page.locator('main')).toBeVisible();
-//   },
-// );
-
-// test(
-//   'list should be visible',
-//   async ({
-//     page,
-//   }) => {
-//     await page.goto(pagePath);
-
-//     await expect(page.locator('ul.List')).toBeVisible();
-//   },
-// );
-
-// test(
-//   'input should be visible',
-//   async ({
-//     page,
-//   }) => {
-//     await page.goto(pagePath);
-
-//     await expect(page.locator('input.ListItem-input')).toBeVisible();
-//   },
-// );
-
-// test(
-//   'add button should be visible',
-//   async ({
-//     page,
-//   }) => {
-//     await page.goto(pagePath);
-
-//     await expect(page.locator('button.ListItem-add')).toBeVisible();
-//   },
-// );
-
 test(
   'added item should be visible at the end of the list',
   async ({
