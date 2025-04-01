aa3f6b913e38cd42b70696aa52173cf8478d80c2
Dominykas Baniulis <DOMBA@danskebank.lt>
2025-04-01 15:25:59 +0300

    pass tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+type Item = {
+  value: string;
+  date: string;
+};
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'test-app2';
+  inputValue: string = '';
+  items: Item[] = [];
+
+  itemDone = false;
+
+  addItem() {
+    const date = new Date();
+    this.items.push({
+      value: this.inputValue,
+      date: date.toDateString(),
+    });
+
+    this.inputValue = '';
+  }
+
+  markAsDone() {
+    this.itemDone = true;
+  }
 }
