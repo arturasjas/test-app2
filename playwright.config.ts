50d6d04fdbd944080a253061adda95738d1bba0d
Vilius Roškus <vroskus@gmail.com>
2025-04-01 14:37:22 +0300

    Minor update

--- playwright.config.ts
+++ playwright.config.ts
@@ -47,8 +47,8 @@ export default defineConfig({
     : [
         { name: 'GoogleChrome', use: {
           launchOptions: {
-              executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-              // executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
+              // executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+              executablePath: 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe',
           },
         } },
         // { name: 'GoogleChrome', use: { ...devices['Desktop Chrome'] } },
