15778da673ec13840f27777afc1c92a151267317
AJAS <ajas@danskebank.lt>
2025-04-01 14:20:22 +0300

    Item tests

--- tests/AJAS.spec.ts
+++ tests/AJAS.spec.ts
@@ -4,23 +4,11 @@ import { test, expect } from '@playwright/test';
 const pagePath = 'http://localhost:4200';
 
 test(
-  'page should be visible',
+  '(AJAS)Items list is visible',
   async ({
     page,
   }) => {
     await page.goto(pagePath);
 
-    await expect(page.locator('main')).toBeVisible();
-  },
-);
-
-test(
-  'page should be visible',
-  async ({
-    page,
-  }) => {
-    await page.goto(pagePath);
-
-    await expect(page.locator('main')).toBeVisible();
-  },
+    await expect(page.locator('li')).toBeTruthy()  },
 );
